ce37ef53f5406ec57c39213347a274e439677838
Victor <victorsetlhare@gmail.com>
2020-05-16 14:54:08 +0100

    add itemDetail template

---
+++ src/components/ItemDetailComponent.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div v-if='reports'>
+      <h3>{{reports.cases}}</h3>
+      <h3>{{reports.recovered}}</h3>
+  </div>
+  
+</template>
+
+<script>
+import {eventBus} from '..main.js';
+export default {
+    name: 'item-detail',
+    props: ['item']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ItemListComponent.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  <li v-on:click="handleClick">{{item.cases}}</li>
+</template>
+
+<script>
+export default {
+  name: 'item-list-components',
+  props: ['reports'],
+  methods: {
+    handleClick(){
+      eventBus.$emit('item-selected', this.item)
+    }
+  }
+  
+}
+</script>
+
+<style>
+</style>

2109ab44ed6af03c31ae7b2363b66a3623cdf038
Victor <victorsetlhare@gmail.com>
2020-05-16 14:14:08 +0100

    add app.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
-<templat
-  
+<template>
+  <div>
+    <h1>Covid-19 Report</h1>
+  </div>
+</template>
+
+<template>
+
 </template>
 
 <script>
-export default {
+  import {eventBus} from './main.js';
 
-}
+  export default {
+    name: 'app',
+      data(){
+        return {
+        reports: [],
+        selectedItem: null
+        };
+      },
+    mounted(){
+      fetch('https://covid19api.io/api/v1/AllReports')
+      .then(res => rs.json())
+      .then(reports => this.reports = reports)
+
+      eventBus.$om('item-selected', (item) => {
+        this.selectedItem = item
+      })
+    },
+    components: {
+      "item-list": ItemList,
+      "item-datail": ItemDetail
+    }
+  }
 </script>
 
 <style>
-
+  .main-container {
+  display: flex;
+  }
 </style>
